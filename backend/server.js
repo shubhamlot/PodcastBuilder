97733430b86cef39adfde6676fa316c1eadcbdd2
shubhamlot <52791419+shubhamlot@users.noreply.github.com>
2021-03-27 10:45:49 +0530

    updated podcast builder

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,52 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid');
 const app = express()
-const mongoose = require('mongoose')
+
 
 const port = 3000
 
 
+let UserList = []
 
-mongoose.connect('mongodb://localhost/PodcastBuilder')
-.then(
-    app.listen(3000)
-).catch(err=>{
-    console.log(err)
+class Room {
+    
+    constructor(roomid,roomName,guests){
+        this.roomid = roomid
+        this.roomName = roomName
+        this.guests=[]
+        this.guests.push(guests)
+
+    }
+
+}
+
+class User {
+    constructor(username){
+        this.username = username
+    }
+}
+
+// app.get(`/`,(req,res)=>{
+//     res.redirect(`/username=Raj&${uuidv4()}`)
+// })
+
+
+
+app.get(`/username=:username&:room&roomname=:roomname`,(req,res)=>{
+    
+    let user = new User(req.params.username)
+    if(UserList.find(roomid)=== roomid){
+      
+    }
+    else{
+    let room = new Room(req.params.room,req.params.roomname,user)
+    }
+    // console.log("user join")
+
+
+   
+    UserList.push(room)
+    console.log(UserList)
 })
+
+app.listen(port)
