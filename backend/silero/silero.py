58460a9d5e6091b0b1fac7a2fe13d10ff41cf0c0
avina25 <68471316+avina25@users.noreply.github.com>
2021-06-06 14:34:55 +0530

    first commit!!

--- backend/silero/silero.py
+++ backend/silero/silero.py
@@ -4,7 +4,6 @@ import torchaudio
 from glob import glob
 
 device = torch.device('cpu')  # gpu also works, but our models are fast enough for CPU
-
 model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
                                        model='silero_stt',
                                        language='en', # also available 'de', 'es'
@@ -12,10 +11,10 @@ model, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models',
 (read_batch, split_into_batches,
  read_audio, prepare_model_input) = utils  # see function signature for details
 
-# download a single file, any format compatible with TorchAudio (soundfile backend)
+# download a single file, any format compatible with TorchAudio
 #torch.hub.download_url_to_file('https://opus-codec.org/static/examples/samples/speech_orig.wav',
-                              # dst ='speech_orig.wav', progress=True)
-test_files = glob('a1.wav')
+                               #dst ='speech_orig.wav', progress=True)
+test_files = glob('right.wav')
 batches = split_into_batches(test_files, batch_size=10)
 input = prepare_model_input(read_batch(batches[0]),
                             device=device)
