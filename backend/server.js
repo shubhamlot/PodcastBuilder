e2e0d78f00e31504a80a5377817ce1cd5061a940
avina25 <68471316+avina25@users.noreply.github.com>
2021-07-01 04:23:33 +0530

    update
    changes!!

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs') //to write the file
 const RSS = require('rss')
 
 const blog = {
@@ -6,10 +6,21 @@ const blog = {
     url:"https://www.w3schools.com",
     discription:"Free web building tutorials",
     author:"free",
-    artcles:[{
-        title:"free",
+    contentType:"Important",
+    country:"India",
+    episodes:[{
+        title:"Episode1",
         url:"https://www.w3schools.com/xml/xml_rss.asp",
+        description:"Based on true story....!!",
+        category:"Entertainment",
         pulishedDate:"June 20, 2020 04:00:00 GMT"
+    },
+    {
+        title:"Episode2",
+        url:"https://www.w3schools.com/xml/xml_rss.asp",
+        description:"Welcome to starting a Podcast",
+        category:"Seminar",
+        pulishedDate:"June 25, 2020 04:00:00 GMT"
     }]
 }
 
@@ -17,18 +28,25 @@ const blog = {
 const feed = new RSS({
     title: blog.title,
     description: blog.discription,
+    author:blog.author,
+    contentType:blog.contentType,
+    country:blog.country,
     feed_url: 'http://example.com/rss.xml',
 
 })
 
-for(const artcles of blog.artcles){
+for(const episodes of blog.episodes){
     feed.item({
-        title:artcles.title,
-        url:artcles.url,
-        date:artcles.pulishedDate
+        title:episodes.title,
+        url:episodes.url,
+        description:episodes.description,
+        category:episodes.category,
+        date:episodes.pulishedDate
     })
 }
 
 
 const xml = feed.xml({indent:true})
 fs.writeFileSync("feed.xml",xml)
+
+
